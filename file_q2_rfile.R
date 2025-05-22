8bd483a1ac8f306872d8b50b9cb1b8fcb54cee22
justin618511kim <125091638+justin618511kim@users.noreply.github.com>
2025-05-21 23:59:27 -0700

    Update file_q2_rfile.R

--- file_q2_rfile.R
+++ file_q2_rfile.R
@@ -1,12 +1,11 @@
-# NOTE: To load data, you must download both the extract's data and the DDI
-# and also set the working directory to the folder with these files (or change the path below).
 library('dplyr')
 library('lubridate')
+library('fixest')
 
 
 if (!require("ipumsr")) stop("Reading IPUMS data into R requires the ipumsr package. It can be installed using the following command: install.packages('ipumsr')")
 
-ddi <- read_ipums_ddi("q2_final_ddifile.xml")
+ddi <- read_ipums_ddi("q2_final_ddi_file.xml")
 raw_data <- read_ipums_micro(ddi)
 industry_Data <- read.csv('indnames.csv')
 
@@ -60,13 +59,33 @@ precovid_retail_employed <- employed_data_Retail %>%
 post_covid_retail_unemployed <- employed_data_Retail %>%
   filter(YEAR >= 2021)
 
-
-
 cut_off <- ymd(c("2020-01-01", "2020-12-31"))
 
-## Regression Model(Employment ~ Time)
+## Regression Model
 
-predict_treatment <- feols()
+regression_data <- main_data %>%
+  filter(YEAR < 2022 | (YEAR == 2022 & MONTH <= 4))
+
+regression_data <- main_data %>%
+  mutate(employed = ifelse(EMPSTAT < 20, 1, 0), 
+         retail = ifelse(between(IND, 4670, 5790), 1, 0),
+         year_month = factor(paste0(YEAR, "-", sprintf("%02d", MONTH))),
+         post_assesment_period = ifelse(YEAR >= 2021, 1, 0))
+
+model_data <- regression_data %>%
+  filter(YEAR <= 2019 | YEAR >= 2021)
+
+main_model <- feols(employed~post_assesment_period * retail | year_month, data = model_data)
+
+pre_covid_data <- subset(regression_data, YEAR <= 2019)
+
+pre_covid_model <- feols(employed~retail | year_month, data = pre_covid_data)
+
+post_covid_data <- subset(regression_data, YEAR >= 2021)
+
+post_covid_model <- feols(employed~retail | year_month, data = post_covid_data)
+
+etable(pre_covid_model, post_covid_model)
 
 
 
